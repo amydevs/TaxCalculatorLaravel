07291b5b1f54caf1beb18e6effce7dcc385fcc1d
Amy Y <ctrlalternativedelete@gmail.com>
2021-05-06 06:19:53 +1000

    updated api

--- public/testing.php
+++ public/testing.php
@@ -1,28 +1,32 @@
 <?php
-$input = 180000;
-$taxToBePaid = 0;
-
-$brackets = [
-    createTaxArray(0, 0.19, 18201),
-    createTaxArray(5092, 0.325, 45001),
-    createTaxArray(29467, 0.37, 120001),
-    createTaxArray(51667, 0.45, 180001)
-];
-$thresholds = array_column($brackets, 'minThreshold');
-array_multisort($thresholds, SORT_DESC, $brackets);
-
-
-foreach ($brackets as $key => $value) {
-    if ($taxToBePaid == 0){
-        $differenceOfBracketandInput = $input-($value["minThreshold"]-1);
-        if ($differenceOfBracketandInput > 0){
-            echo json_encode($value);
-            $taxToBePaid = ($differenceOfBracketandInput * $value["perDollarTax"]) + $value["baseTax"];
+class Calculator {
+    function calculate($input) {
+        function createTaxArray($baseTax, $perDollarTax, $minThreshold) {
+            return array("baseTax"=>$baseTax, "perDollarTax"=>$perDollarTax, "minThreshold"=>$minThreshold);
         }
+
+        $taxToBePaid = 0;
+
+        $brackets = [
+            createTaxArray(0, 0.19, 18201),
+            createTaxArray(5092, 0.325, 45001),
+            createTaxArray(29467, 0.37, 120001),
+            createTaxArray(51667, 0.45, 180001)
+        ];
+        $thresholds = array_column($brackets, 'minThreshold');
+        array_multisort($thresholds, SORT_DESC, $brackets);
+
+
+        foreach ($brackets as $key => $value) {
+            if ($taxToBePaid == 0){
+                $differenceOfBracketandInput = $input-($value["minThreshold"]-1);
+                if ($differenceOfBracketandInput > 0){
+                    echo json_encode($value);
+                    $taxToBePaid = ($differenceOfBracketandInput * $value["perDollarTax"]) + $value["baseTax"];
+                }
+            }
+        }
+        return $taxToBePaid;
     }
 }
-echo $taxToBePaid;
 
-function createTaxArray($baseTax, $perDollarTax, $minThreshold) {
-    return array("baseTax"=>$baseTax, "perDollarTax"=>$perDollarTax, "minThreshold"=>$minThreshold);
-}
