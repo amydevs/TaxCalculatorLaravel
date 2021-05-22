94393d2684ec8c9705fc7344321aee058a7945d6
Amy Y <ctrlalternativedelete@gmail.com>
2021-05-22 12:03:31 +1000

    added scroll 2 top button

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,5 @@
 <div id="footer"><span>© Copyright 2021 AyanAmy</span></div>
+<button onclick="javascript:document.body.scrollTop = 0; document.documentElement.scrollTop = 0;" id="scroll2Top" title="Go to top">^</button>
 
 <style>
     #footer {
@@ -13,4 +14,43 @@
         background-clip: text;
         -webkit-text-fill-color: transparent;
     }
+    #scroll2Top {
+        display: none;
+        position: fixed;
+        bottom: 2vh;
+        right: 0;
+        z-index: 99;
+        border: none;
+        background-color: black;
+        color: white;
+        width: 6vh;
+        height: 6vh;
+        cursor: pointer;
+        justify-content: center;
+        align-items: center;
+        border-radius: 100%;
+        font-size: 50pt;
+        transition: var(--easeTransition);
+    }
+
+    #myBtn:hover {
+        background-color: #555; /* Add a dark-grey background on hover */
+    }
 </style>
+
+<script>
+    // Initialize button
+    scrollButton = document.getElementById("scroll2Top");
+    var defaultState = "translateX(10vh) rotate(100deg)"
+    scrollButton.style.transform = defaultState;
+    scrollButton.style.display = "flex";
+
+    document.addEventListener('scroll', (e) => {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollButton.style.transform = "translateX(-2vh)";
+        }
+        else {
+            scrollButton.style.transform = defaultState;
+        }
+    })
+</script>
