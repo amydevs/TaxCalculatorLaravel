3f7df39727d2310be34423a72e2e6ee72411b284
Amy Y <ctrlalternativedelete@gmail.com>
2021-05-23 19:37:20 +1000

    made it a pwa

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,5 +1,12 @@
+{{-- Basic Meta Tags --}}
 <meta charset="utf-8">
 <meta name="description" content="">
 <meta name="author" content="AyanAmy">
+
+{{-- Links to External/Interal Resources --}}
+<link rel="icon" href="assets/icons/calculator_icon_192.png" type="image/png">
+{{-- PWA Stuff --}}
+<link rel="manifest" href="assets/manifest.json">
+<link rel="apple-touch-icon" href="assets/icons/calculator_icon_192.png">
 
 <title>Tax Calculator</title>
