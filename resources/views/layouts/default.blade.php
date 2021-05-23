48c3d5fee6da7b63f375e7d4940be5b994d2c0e5
Amy Y <ctrlalternativedelete@gmail.com>
2021-05-23 11:51:19 +1000

    added viewport 4 mobile

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,6 +4,7 @@
             @include('includes.head')
             @yield('head')
             <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
         </head>
         <body>
             <div class="mainContainer scrollSnapStart repeatableHeight">
