0bf340f447a791f73dae3280c56bb63cac787ca5
Amy <ctrlalternativedelete@gmail.com>
2021-05-25 12:16:52 +1000

    moved css to blade

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -28,50 +28,6 @@
     @endif
 
     <style>
-        /* form styling */
-        #contactForm * {
-            transition: var(--easeTransition);
-        }
-        #contactForm {
-            display: flex;
-            align-items: stretch;
-            justify-content: stretch;
-            flex-direction: column;
-            -moz-column-gap: 10px;
-                column-gap: 10px;
-            row-gap: 10px;
-        }
-        #contactForm label {
-            width: -webkit-max-content;
-            width: -moz-max-content;
-            width: max-content;
-        }
-        #contactForm button {
-            background: var(--layerWidgetBackground);
-            border: none;
-            padding: 5px;
-            border-radius: var(--universalBorderRadius);
-            box-shadow: var(--widgetBoxShadows);
-        }
-        #contactForm button:hover {
-            cursor: pointer;
-            background-color: var(--hoverColour);
-            color: var(--defaultBackground);
-            box-shadow: var(--widgetHoverBoxShadows);
-        }
-        #taxableIncome {
-            box-shadow: var(--widgetBoxShadows);
-        }
-        #taxableIncome:focus {
-            box-shadow: var(--widgetHoverBoxShadows);
-        }
-        input[type=text], textarea {
-            box-shadow: var(--widgetBoxShadows);
-        }
-        input[type=text]:focus, textarea:focus {
-            box-shadow: var(--widgetHoverBoxShadows);
-        }
-
         /* basic styling */
         .repeatableContent {
             display: flex;
