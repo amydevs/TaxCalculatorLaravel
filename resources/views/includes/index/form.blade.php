06644956ba80c69e99c8432759f522a38c67c760
Amy Y <ctrlalternativedelete@gmail.com>
2021-05-25 15:15:26 +1000

    Revert "added moving css to section"
    This reverts commit c59090a21315b375022fd36a700d2eded5a237ea.

--- resources/views/includes/index/form.blade.php
+++ resources/views/includes/index/form.blade.php
@@ -6,48 +6,45 @@
     </form>
 </div>
 
-@section('head')
-    @parent
-    <style>
-        #calcform * {
-            transition: var(--easeTransition);
-        }
-        #calcform {
-            display: grid;
-            grid-template-columns: auto auto;
-            align-items: center;
-            justify-content: center;
-            padding: 10px;
-            -moz-column-gap: 10px;
-                column-gap: 10px;
-            row-gap: 10px;
-            border-radius: var(--universalBorderRadius);
-            background: var(--layerWidgetBackground);
-            box-shadow: var(--widgetBoxShadows);
-        }
-        #calcform label {
-            width: -webkit-max-content;
-            width: -moz-max-content;
-            width: max-content;
-        }
-        #calcform button {
-            background: var(--layerWidgetBackground);
-            border: none;
-            padding: 5px;
-            border-radius: var(--universalBorderRadius);
-            box-shadow: var(--widgetBoxShadows);
-        }
-        #calcform button:hover {
-            cursor: pointer;
-            background-color: var(--hoverColour);
-            color: var(--defaultBackground);
-            box-shadow: var(--widgetHoverBoxShadows);
-        }
-        #taxableIncome {
-            box-shadow: var(--widgetBoxShadows);
-        }
-        #taxableIncome:focus {
-            box-shadow: var(--widgetHoverBoxShadows);
-        }
-    </style>
-@endsection
+<style>
+    #calcform * {
+        transition: var(--easeTransition);
+    }
+    #calcform {
+        display: grid;
+        grid-template-columns: auto auto;
+        align-items: center;
+        justify-content: center;
+        padding: 10px;
+        -moz-column-gap: 10px;
+            column-gap: 10px;
+        row-gap: 10px;
+        border-radius: var(--universalBorderRadius);
+        background: var(--layerWidgetBackground);
+        box-shadow: var(--widgetBoxShadows);
+    }
+    #calcform label {
+        width: -webkit-max-content;
+        width: -moz-max-content;
+        width: max-content;
+    }
+    #calcform button {
+        background: var(--layerWidgetBackground);
+        border: none;
+        padding: 5px;
+        border-radius: var(--universalBorderRadius);
+        box-shadow: var(--widgetBoxShadows);
+    }
+    #calcform button:hover {
+        cursor: pointer;
+        background-color: var(--hoverColour);
+        color: var(--defaultBackground);
+        box-shadow: var(--widgetHoverBoxShadows);
+    }
+    #taxableIncome {
+        box-shadow: var(--widgetBoxShadows);
+    }
+    #taxableIncome:focus {
+        box-shadow: var(--widgetHoverBoxShadows);
+    }
+</style>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,13 +1,14 @@
 <!doctype html>
     <html>
         <head>
-            @include('includes.head')
-            @yield('head')
-
             {{-- This is just for mobile chrome cos scroll snapping is broken there. --}}
             <style>html { scroll-snap-type: {{ preg_match('/(Android)((.|\n)*)(Chrome)((.|\n)*)(Mobile)/', request()->header('User-Agent')) >= 1 ? "none !important;" : "y proximity;" }} }</style>
+            @include('includes.head')
+            @yield('head')
             <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
         </head>
         <body>
             <div class="mainContainer scrollSnapStart repeatableHeight">
