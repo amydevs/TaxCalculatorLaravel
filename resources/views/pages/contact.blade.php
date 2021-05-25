569d6da57a2136c76dab6dafddf3a6d1abacf2f4
Amy <ctrlalternativedelete@gmail.com>
2021-05-25 12:09:37 +1000

    fixed contact form style

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -8,7 +8,7 @@
 
             <input type="text" id="lname" name="lname" placeholder="Last Name">
 
-            <textarea id="body" name="body" placeholder="Type Your Message Here"></textarea>
+            <textarea id="body" name="body" style="width: 75vw; height: 40vh; min-width: 300px;" placeholder="Type Your Message Here"></textarea>
 
             <button type="submit" value="Submit" style="grid-column: 1 / span 2;">Submit</button>
         </form>
