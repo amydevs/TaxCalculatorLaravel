06a1a8e410ca3867126a9719d9fa2d95eb55502c
Amy <ctrlalternativedelete@gmail.com>
2021-05-26 08:31:14 +1000

    made contact forms required

--- resources/views/includes/contact/form.blade.php
+++ resources/views/includes/contact/form.blade.php
@@ -1,10 +1,10 @@
 <form id="contactForm" method="POST" action="{{ Request::getRequestUri() }}">
     @csrf
-    <input type="text" id="fname" name="fname" placeholder="First Name" value="{{ request()->get("fname") }}">
+    <input type="text" id="fname" name="fname" placeholder="First Name" value="{{ request()->get("fname") }}" required>
 
-    <input type="text" id="lname" name="lname" placeholder="Last Name" value="{{ request()->get("lname") }}">
+    <input type="text" id="lname" name="lname" placeholder="Last Name" value="{{ request()->get("lname") }}" required>
 
-    <textarea id="body" name="body" style="width: 75vw; height: 40vh; min-width: 300px;" placeholder="Type Your Message Here"></textarea>
+    <textarea id="body" name="body" style="width: 75vw; height: 40vh; min-width: 300px;" placeholder="Type Your Message Here" required></textarea>
 
     <button type="submit" value="Submit" style="grid-column: 1 / span 2;">Submit</button>
 </form>
