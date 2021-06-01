130ddee69310231b5b7046c02cea43f2ee18cf1f
Amy <ctrlalternativedelete@gmail.com>
2021-06-02 09:18:12 +1000

    disabled autofill

--- resources/views/includes/index/form.blade.php
+++ resources/views/includes/index/form.blade.php
@@ -1,5 +1,5 @@
 <div class="calculatorFormDiv">
-    <form method="POST" action="{{ Request::getRequestUri() }}#about" id="calcform">
+    <form method="POST" action="{{ Request::getRequestUri() }}#about" id="calcform" autocomplete="off">
         @csrf
         <input style="grid-column: 1 / span 2;" type="text" id="taxableIncome" name="taxableIncome" placeholder="Enter Taxable Income" value='{{ request()->get("taxableIncome") }}' required>
         <button style="grid-column: 1 / span 2;" type="submit" form="calcform" value="submit">Submit</button>
