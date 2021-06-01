1bc868c27534371818929f57cb30aa8c5bfbc0dc
Amy Y <ctrlalternativedelete@gmail.com>
2021-06-01 22:42:56 +1000

    made table better

--- resources/views/includes/index/table.blade.php
+++ resources/views/includes/index/table.blade.php
@@ -1,45 +1,48 @@
-<div class="tableContainer">
-    <table class="taxTable">
-        <thead>
+<table class="taxTable">
+    <thead>
+        <tr>
+            <th>Income Thresholds</th>
+            <th>Base Tax</th>
+            <th>Rate</th>
+            <th>Tax Payable on This Income</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($taxBrackets as $key=>$bracket)
             <tr>
-                <th>Income Thresholds</th>
-                <th>Base Tax</th>
-                <th>Rate</th>
-                <th>Tax Payable on This Income</th>
+                <td>${{ $bracket["minThreshold"] }} {{ array_key_exists($key+1, $taxBrackets) ? "to $".($taxBrackets[$key+1]["minThreshold"] - 1) : "and Over" }}</td>
+                <td>${{ $bracket["baseTax"] }}</td>
+                <td>{{ $bracket["perDollarTax"] * 100 }}%</td>
+                <td>${{ $bracket["baseTax"] }} plus ${{ $bracket["perDollarTax"] }} for each $1 over ${{ $bracket["minThreshold"] - 1 }}</td>
             </tr>
-        </thead>
-        <tbody>
-            @foreach ($taxBrackets as $key=>$bracket)
-                <tr>
-                    <td>${{ $bracket["minThreshold"] }} {{ array_key_exists($key+1, $taxBrackets) ? "to $".($taxBrackets[$key+1]["minThreshold"] - 1) : "and Over" }}</td>
-                    <td>${{ $bracket["baseTax"] }}</td>
-                    <td>{{ $bracket["perDollarTax"] * 100 }}%</td>
-                    <td>${{ $bracket["baseTax"] }} plus ${{ $bracket["perDollarTax"] }} for each $1 over ${{ $bracket["minThreshold"] - 1 }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+        @endforeach
+    </tbody>
+</table>
+
 
 <style>
-    .tableContainer {
+    .taxTable {
         flex: 1;
+        flex-grow: 1;
+        border-collapse:separate;
+        border: solid var(--textColour) 4px;
         border-radius: var(--universalBorderRadius);
-        overflow: hidden;
-        border: 4px solid var(--textColour);
         box-shadow: var(--widgetBoxShadows);
         font-size: var(--specifiedFontSize);
+        border-spacing: 0;
+        overflow: hidden;
     }
-    .tableContainer > table, th, td {
-        border: 1px solid var(--textColour);
+    .taxTable td, th {
+        flex: 1;
+        border-left: solid var(--textColour) 2px;
+        border-top: solid var(--textColour) 2px;
         text-align: center;
         padding: 6px;
     }
-    .tableContainer table {
-        height: 100%;
-        width: 100%;
-        transform: scaleX(1.007) scaleY(1.007);
-        border-radius: var(--universalBorderRadius);
-        border-collapse: collapse;
+    .taxTable th {
+        border-top: none;
+    }
+    .taxTable td:first-child, th:first-child {
+        border-left: none;
     }
 </style>
