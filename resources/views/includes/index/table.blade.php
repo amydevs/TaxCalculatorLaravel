9fd2351956ceafbffdfc26239a07e59b353478cf
Amy <ctrlalternativedelete@gmail.com>
2021-06-18 12:07:45 +1000

    fixed number format

--- resources/views/includes/index/table.blade.php
+++ resources/views/includes/index/table.blade.php
@@ -13,7 +13,7 @@
                 <td>${{ number_format($bracket["minThreshold"]) }} {{ array_key_exists($key+1, $taxBrackets) ? "to $".number_format($taxBrackets[$key+1]["minThreshold"] - 1) : "and Over" }}</td>
                 <td>${{ number_format($bracket["baseTax"]) }}</td>
                 <td>{{ $bracket["perDollarTax"] * 100 }}%</td>
-                <td>${{ number_format($bracket["baseTax"]) }} plus ${{ number_format($bracket["perDollarTax"]) }} for each $1 over ${{ number_format($bracket["minThreshold"] - 1) }}</td>
+                <td>${{ number_format($bracket["baseTax"]) }} plus ${{ $bracket["perDollarTax"] }} for each $1 over ${{ number_format($bracket["minThreshold"] - 1) }}</td>
             </tr>
         @endforeach
     </tbody>
