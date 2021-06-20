ed9b159f6f7b9665a4666d269f7e836d30de5809
Amy Y <ctrlalternativedelete@gmail.com>
2021-06-20 19:13:35 +1000

    fixed logical error

--- resources/views/includes/index/table.blade.php
+++ resources/views/includes/index/table.blade.php
@@ -15,7 +15,10 @@
                 <td>${{ number_format($bracket["minThreshold"]) }} {{ array_key_exists($key+1, $taxBrackets) ? "to $".number_format($taxBrackets[$key+1]["minThreshold"] - 1) : "and Over" }}</td>
                 <td>${{ number_format($bracket["baseTax"]) }}</td>
                 <td>{{ $bracket["perDollarTax"] * 100 }}%</td>
-                <td>${{ number_format($bracket["baseTax"]) }} plus ${{ $bracket["perDollarTax"] }} for each $1 over ${{ number_format($bracket["minThreshold"] - 1) }}</td>
+                <td>
+                    ${{ number_format($bracket["baseTax"]) }} plus ${{ $bracket["perDollarTax"] }} for each $1 over ${{ number_format($bracket["minThreshold"] - 1) }}
+                    {{ array_key_exists($key+1, $taxBrackets) ? "and under $".number_format($taxBrackets[$key+1]["minThreshold"] - 1) : "" }}
+                </td>
             </tr>
         @endforeach
     </tbody>
